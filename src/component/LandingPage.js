4d6c230b9bd636d431865de1c75f5e69a21029a7
shashi <mallepally.varma@jsw.in>
2023-08-10 21:29:36 +0530

    basic registraion

---
+++ src/component/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LandingPage() {
+    return (
+        <div>
+            <h1>Welcome to Our Website!</h1>
+            <p>Thank you for joining us. Explore our services and features.</p>
+        </div>
+    );
+}
+
+export default LandingPage;
