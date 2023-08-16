d8203236bc85e6059cb3fcfd02e34280da5d164f
shashi <mallepally.varma@jsw.in>
2023-08-16 10:18:31 +0530

    refactor

--- src/component/ChatRoom.js
+++ src/component/ChatRoom.js
@@ -1,63 +1,61 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const ChatRoom = ({ currentUser }) => {
+const ChatRoom = () => {
+    const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     const [webSocket, setWebSocket] = useState(null);
 
+    const connectWebSocket = () => {
+        const ws = new WebSocket('ws://localhost:8080/ws/chat');
+        ws.onmessage = (event) => {
+            const receivedMessage = event.data;
+            setMessages([...messages, receivedMessage]);
+        };
+        setWebSocket(ws);
+    };
+
+    const disconnectWebSocket = () => {
+        if (webSocket) {
+            webSocket.close();
+        }
+    };
+
+    const sendMessage = () => {
+        if (webSocket && message.trim() !== '') {
+            webSocket.send(message);
+            setMessage('');
+        }
+    };
+
     useEffect(() => {
-        const socket = new WebSocket('ws://localhost:8080/ws'); // Replace with your WebSocket server URL
-
-        socket.onopen = () => {
-            console.log('WebSocket connection established.');
-            setWebSocket(socket);
-        };
-
-        socket.onmessage = event => {
-            const messageData = JSON.parse(event.data);
-            setMessages(prevMessages => [...prevMessages, messageData]);
-        };
-
-        socket.onclose = () => {
-            console.log('WebSocket connection closed.');
-        };
-
-        return () => {
-            if (socket) {
-                socket.close();
-            }
-        };
+        fetchMessages();
+        connectWebSocket();
+        return () => disconnectWebSocket();
     }, []);
 
-    const sendMessage = message => {
-        if (webSocket) {
-            const messageObject = {
-                sender: currentUser,
-                content: message
-            };
-            webSocket.send(JSON.stringify(messageObject));
-        }
+    const fetchMessages = async () => {
+        const response = await axios.get('/chat/messages');
+        setMessages(response.data);
     };
 
     return (
         <div>
             <div className="message-list">
-                {messages.map((message, index) => (
+                {messages.map((msg, index) => (
                     <div key={index} className="message">
-                        <strong>{message.sender}: </strong>
-                        {message.content}
+                        {msg}
                     </div>
                 ))}
             </div>
-            <input
-                type="text"
-                placeholder="Type your message..."
-                onKeyDown={event => {
-                    if (event.key === 'Enter') {
-                        sendMessage(event.target.value);
-                        event.target.value = '';
-                    }
-                }}
-            />
+            <div className="message-input">
+                <input
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button onClick={sendMessage}>Send</button>
+            </div>
         </div>
     );
 };
