472c63fe2922606a75caf0059527d1c98c0b0daf
shashi <mallepally.varma@jsw.in>
2023-09-06 22:24:15 +0530

    fix

--- src/component/LandingScreen.jsx
+++ src/component/LandingScreen.jsx
@@ -5,7 +5,7 @@ import {useNavigate} from "react-router-dom"; // Import your CSS file for stylin
 const LandingScreen = () => {
     const navigate = useNavigate();
     const handleLogoClick = () => {
-        navigate('/landing-page');
+        navigate('/register');
     };
 
     return (
