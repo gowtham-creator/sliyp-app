b07eedf8dc023ab317f0926e9a0e29866c73833c
shashikanth <shashikanth@JSWs-Laptop.local>
2023-10-20 16:32:25 +0530

    fix the issues

--- src/components/AIFriend.jsx
+++ src/components/AIFriend.jsx
@@ -1,13 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { stackoverflowDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { stackoverflowLight,stackoverflowDark,docco, xcode, vs2015 } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import '../css/AIFriend.css';
+import {solarizedlight} from "react-syntax-highlighter/src/styles/prism";
 
 function AIFriend() {
     const [prompt, setPrompt] = useState('');
     const [response, setResponse] = useState('');
     const [loading, setLoading] = useState(false);
+    const [codeHighlightSettings, setCodeHighlightSettings] = useState({
+        language: 'python',
+        style: stackoverflowDark,
+        customStyle: { fontSize: '11px' },
+        showLineNumbers: true,
+        wrapLines: true,
+    });
+
+    const themes = {
+        'StackOverflow Light' : stackoverflowLight,
+        'StackOverflow Dark' : stackoverflowDark,
+        'Solarized Light': solarizedlight,
+        'Docco': docco,
+        'Xcode': xcode,
+        'VS 2015': vs2015,
+    };
 
     const handlePromptChange = (e) => {
         setPrompt(e.target.value);
@@ -16,6 +33,7 @@ function AIFriend() {
     const handleGenerateText = async () => {
         setLoading(true); // Show the loader while fetching data
         try {
+            // Make your API call here
             const apiUrl = 'https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=AIzaSyA8ldCM9qJ4Y0Eu7Nvn5noyfOgYpTcDkNs';
             const requestData = {
                 prompt: {
@@ -39,6 +57,10 @@ function AIFriend() {
         }
     };
 
+    const handleSettingChange = (name, value) => {
+        setCodeHighlightSettings({ ...codeHighlightSettings, [name]: value });
+    };
+
     return (
         <div className="container">
             <h1>AI Friend</h1>
@@ -54,19 +76,63 @@ function AIFriend() {
             <div>
                 <button onClick={handleGenerateText}>Generate Text</button>
             </div>
+            <div className="response-settings">
+                <h2>Code Highlight Settings</h2>
+                <form>
+                    <div>
+                        <label htmlFor="language">Language:</label>
+                        <select
+                            id="language"
+                            value={codeHighlightSettings.language}
+                            onChange={(e) => handleSettingChange("language", e.target.value)}
+                        >
+                            <option value="javascript">JavaScript</option>
+                            <option value="python">Python</option>
+                            {/* Add more language options */}
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="style">Theme:</label>
+                        <select
+                            id="style"
+                            value={codeHighlightSettings.style}
+                            onChange={(e) => handleSettingChange("style", themes[e.target.value])}
+                        >
+                            {Object.keys(themes).map((theme) => (
+                                <option key={theme} value={theme}>
+                                    {theme}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="showLineNumbers">Show Line Numbers:</label>
+                        <input
+                            type="checkbox"
+                            id="showLineNumbers"
+                            checked={codeHighlightSettings.showLineNumbers}
+                            onChange={(e) => handleSettingChange("showLineNumbers", e.target.checked)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="wrapLines">Wrap Lines:</label>
+                        <input
+                            type="checkbox"
+                            id="wrapLines"
+                            checked={codeHighlightSettings.wrapLines}
+                            onChange={(e) => handleSettingChange("wrapLines", e.target.checked)}
+                        />
+                    </div>
+                </form>
+            </div>
             <div className="generated-text">
                 {loading ? (
                     <div className="loader"></div>
-                ) :
-                    // typeof response === 'string' ? (
-                    <SyntaxHighlighter language="python" style={stackoverflowDark} customStyle={{ fontSize: '11px' }}>
-                        {response.replace("```","//")}
+                ) : (
+                    <SyntaxHighlighter {...codeHighlightSettings}>
+                        {response}
                     </SyntaxHighlighter>
-                // )
-                //         : (
-                //     <pre>{JSON.stringify(response, null, 2)}</pre>
-                // )
-                }
+                )}
             </div>
         </div>
     );
